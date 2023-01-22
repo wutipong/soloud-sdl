fae8069741873a714081a4b6f93f283e6fc55b1f
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2023-01-22 17:34:50 +0700

    Implement ImGUI Window

--- main.cpp
+++ main.cpp
@@ -23,20 +23,50 @@ int main(int argc, char **argv)
         SDL_Event event;
         if (SDL_PollEvent(&event))
         {
+
+            ImGui_ImplSDL2_ProcessEvent(&event);
             if (event.type == SDL_QUIT)
             {
                 break;
             }
         }
-        
+
         SDL_RenderClear(renderer);
         ImGui_ImplSDL2_NewFrame(window);
         ImGui_ImplSDLRenderer_NewFrame();
 
         ImGui::NewFrame();
-        // TODO: Add UI code here.
 
-        ImGui::EndFrame();
+        {
+            int width, height;
+            SDL_GetWindowSize(window, &width, &height);
+
+            ImGui::SetNextWindowPos({0, 0});
+            ImGui::SetNextWindowSize({static_cast<float>(width), static_cast<float>(height)});
+
+            ImGui::Begin("main window", nullptr,
+                         ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+                             ImGuiWindowFlags_NoTitleBar);
+            {
+                if(ImGui::BeginMenuBar())
+                {
+                    if(ImGui::BeginMenu("File"))
+                    {
+                        ImGui::MenuItem("Open");
+                        ImGui::Separator();
+                        if (ImGui::MenuItem("Exit", "Alt+F4"))
+                        {
+                            SDL_Event quit_event = {.type = SDL_QUIT};
+                            SDL_PushEvent(&quit_event);
+                        }
+                        ImGui::EndMenu();
+                    }
+                    ImGui::EndMenuBar();
+                }
+                ImGui::End();
+            }
+            ImGui::EndFrame();
+        }
         ImGui::Render();
         ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
         SDL_RenderPresent(renderer);
